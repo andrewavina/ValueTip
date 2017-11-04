d8305511eed88eeacd136ed06467fd6cc64f8e3e
Andrew Avina <avinaandrew@gmail.com>
2017-11-03 23:53:10 -0700

    set up const's in server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-//mongoose
+const
+    dotenv = require('dotenv').load(),
+    express = require('express'),
+    app = express(),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/stock_app',
+    PORT = process.env.PORT || 3001, //either the port the webhost (e.g. Heroku) assigns if deployed or if local run on port number
+    usersRoutes = require('./routes/users.js')
+
+
