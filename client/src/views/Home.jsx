d1a4b97aa5eb542a30279c871e06e443d01520cd
Andrew Avina <avinaandrew@gmail.com>
2017-11-06 14:05:01 -0800

    added home view

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = (props) => {
+    return (
+        <div className='Home'>
+            <h1>Welcome to The Stock App</h1>
+        </div>
+    )
+}
+
+export default Home
