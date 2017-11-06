fb0928c49bef6ea7af9f76bcfb7a1e4cd3ff9691
Andrew Avina <avinaandrew@gmail.com>
2017-11-05 19:55:04 -0800

    half finished controller/users.js file based off example

---
+++ controllers/users.js
@@ -0,0 +1,32 @@
+const 
+    User = require('../models/User.js'),
+    signToken = require('../serverAuth.js').signToken
+
+module.exports = {
+    //list all users - this should not be publicly available - only visible to site admin if anyone. Company index would be OK publicly.
+    index: (req, res) => {
+        User.find({}, (err, users) => {
+            res.json(users)
+        })
+    },
+
+    //get one user
+    show: (req, res) => {
+        console.log("Current User:") //for testing to make sure correct user is logged in/Current User
+        console.log(req.user) //same as above - see request being sent
+        User.findById(req.params.id, (err, user) => {
+            res.json(user)
+        })
+    },
+
+    //create a new user
+    create: (req, res) => {
+        User.create(req.body, (err, user) => {
+            if(err) return res.json({success: false, code: err.code})
+            //once user is created, generate a token to "log in":
+            const token = signToken(user)
+            res.json({success: true, message: "User created. Token attached.", token})
+        })
+    },
+
+} //should be last closing bracket
