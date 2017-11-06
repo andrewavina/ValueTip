17882b4b9c2bc0a48fc7d559944278ff2ab0a40e
Andrew Avina <avinaandrew@gmail.com>
2017-11-05 18:20:11 -0800

    added User.js file to Models folder. And filled out file based off of example. No name for user.

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const
+    mongoose = require('mongoose'),
+    bcrypt = require('bcrypt-nodejs'),
+    userSchema = new mongoose.Schema({ //email and password are only requirements to create user
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true }
+    })
+
+//adds a method to a user document object to create a hashed password
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(6)) //hashing algorithm will run 6 times
+}
+
+// adds a method to a user document object to check if provided password is correct
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password)
+}
+
+// middleware: before saving, check if password was changed. If so, encrypt new password before saving:
+userSchema.pre('save', function(next) {
+    if(this.isModified('password')) {
+        this.password = this.generateHash(this.password)
+    }
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
