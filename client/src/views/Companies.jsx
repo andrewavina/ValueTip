78ce7e98d9c57b70945cc3dc1d6c82ef9647ad65
Andrew Avina <avinaandrew@gmail.com>
2017-11-07 11:29:41 -0800

    changed companies view back to example layout

--- client/src/views/Companies.jsx
+++ client/src/views/Companies.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
 
-class Companies extends React.Component {
-    state = { currentUser: null }
-
-    componentDidMount() {
-        this.setState({
-            currentUser: this.props.currentUser
-        })
-    }
-    
-    render() {
-        return (
-            <div className='companies'>
-                <h1>Companies Page</h1>
-            </div>
-        )
-    }
+const Companies = (props) => {
+    return (
+        <div className='companies'>
+            <h1>Companies Page</h1>
+        </div>
+    )
 }
 
 export default Companies
