32d990596370a10680bd39ac827c1f1f532f57e2
Andrew Avina <avinaandrew@gmail.com>
2017-11-08 09:51:37 -0800

    updated Settings page to have comp and state to add edit user functionality

--- client/src/views/Settings.jsx
+++ client/src/views/Settings.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
+//import clientAuth from '../clientAuth'
 
-const Settings = (props) => {
-    return (
-        <div className='settings'>
-            <form>
-                <input type="password" placeholder="Password" name="password" />                   
-                <button>Change Password</button>
-            </form>
-            <br/>
-            <button>
-                <a href="/deleteuser">Delete Account</a>
-            </button>
-        </div>
-    )
+class Settings extends React.Component {
+    state = {
+        fields: { password: ''}
+    }
+
+    
+
+    render() {
+        const { password } = this.state.fields 
+            return (
+                <div className='settings'>
+                    <form>
+                        <input type="password" placeholder="Password" name="password" />                   
+                        <button>Change Password</button>
+                    </form>
+                    <br/>
+                    <button>
+                        <a href="/deleteuser">Delete Account</a>
+                    </button>
+                </div>
+            )
+    }
 }
 
 export default Settings
