b89583cc88a1904340f9980379b8280c950f8ac2
Andrew Avina <avinaandrew@gmail.com>
2017-11-09 14:15:32 -0800

    updated myreport view to render new stocks components

--- client/src/views/MyReport.jsx
+++ client/src/views/MyReport.jsx
@@ -1,86 +1,49 @@
-import React from 'react';
-import Item from './Stock';
+import React from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class MyReport extends React.Component {
-    
-    state = {
-        newCompanyTitle: '', 
-        stocklist: [
-            { company: "ABC", ticker: "A", price: 34, _id: "293023" },
-            { company: "CBS", ticker: "C", price: 183, _id: "123211" },
-            { company: "NBC", ticker: "N", price: 8, _id: "423232" }            
-        ]
+    constructor(props){
+        super(props)
+        this.state = {
+            stocks: []
+        }
     }
 
-  
-    onFormSubmit(evt) {
-        console.log("Submitting form...")
-        evt.preventDefault()
-        this.setState({
-            newItemTitle: '',
-            stocklist: [
-                ...this.state.stocklist,
-                { company: this.state.newItemTitle, _id: Math.random() * 10000 }
-            ]
+    getStocks(){
+        axios({
+            method: 'get',
+            url: `/api/users/${this.props.currentUser._id}/stocks`
+        }).then((res) => {
+            this.setState({
+                stocks: res.data
+            })
         })
     }
 
-    // use bind(this) in render() function to tell react to access function
-    onInputChange(evt){
-        this.setState({search: evt.target.value})
+    componentDidMount(){
+        this.getEvents()
     }
 
-    onRemoveClick(id){
-        console.log("removing an item...")
-        console.log(id)
-        this.setState({
-            stocklist: this.state.stocklist.filter((item) => {
-                return item._id !== id
-            }) //filter is similar to map. Only items that include truthy will be included in new array
-        })
-    }
-
-    render() {
-        let filteredCompanies = this.state.stocklist.filter(
-            (stock) => {
-                return stock.company.indexOf(this.state.search) !== -1;
-            }
-        )
+    render(){
+        console.log(this.props.currentUser.email)
         return (
             <div className="MyReport">
                 <h1>My Report</h1>
-
-                <h2>Company Search:</h2>
-               <form onSubmit={this.onFormSubmit.bind(this)}>
-                    {/* <input type="text" 
-                        value={this.state.search}
-                        onChange={this.onInputChange.bind(this)}
-                     /> */}
-                    <button>Add Stock</button>
-                </form>
-
-                <h2>Saved Stocks:</h2>
-                
-                <ul>
-                   {/* placeholder for saved/bookmarked stocks */}
-                </ul>
-
-                {/* Below is test list to render list of stocks just for testing */}
-                {/* <ul>
-                    {this.state.stocklist.map((item) => { 
-                        return (
-                            <Item 
-                                key={item._id}
-                                item={item}
-                                onRemoveClick={this.onRemoveClick.bind(this)}
-                            />
-                        )
-                    })}
-                </ul> */}
+                <Link to="/create-stock">Add a Stock</Link>
+                <hr/>
+                <h2>Saved Stocks</h2>
+                    <ul>
+                        {this.state.events.map((stock, index) => {
+                            return (
+                                <li key={stock._id}><Link to={`/show-stock${stock._id}`} key={index}>{stock.name}</Link></li>
+                            ) 
+                        })}
+                    </ul>
             </div>
         )
     }
-    
-} // last bracket
+
+} //last bracket
 
 export default MyReport
