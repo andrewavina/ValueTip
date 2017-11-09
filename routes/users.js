611ce4b1436fdd65d7894c18cffecf7f6cc473b1
Andrew Avina <avinaandrew@gmail.com>
2017-11-09 15:09:41 -0800

    updated user routes to handle :id/stocks calls

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const
     express = require('express'),
     usersRouter = new express.Router(), 
     usersCtrl = require('../controllers/users.js'),
+    stocksCtrl = require('../controllers/stocks.js'),
     verifyToken = require('../serverAuth.js').verifyToken
 
 usersRouter.route('/')
@@ -15,5 +16,9 @@ usersRouter.route('/:id')
     .get(usersCtrl.show)
     .patch(usersCtrl.update)
     .delete(usersCtrl.destroy)
+
+usersRouter.route('/:id/stocks')
+.get(stocksCtrl.index)
+.post(stocksCtrl.create)
 
 module.exports = usersRouter
