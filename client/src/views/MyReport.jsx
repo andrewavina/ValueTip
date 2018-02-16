1cf5883ccceb53a215587c9cf9c31d91c21ec2e3
Andrew Avina <avinaandrew@gmail.com>
2018-02-15 19:04:46 -0800

    updated my report page

--- client/src/views/MyReport.jsx
+++ client/src/views/MyReport.jsx
@@ -6,7 +6,8 @@ class MyReport extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            stocks: []
+            stocks: [],
+            companyImage: ""
         }
     }
 
@@ -16,7 +17,8 @@ class MyReport extends React.Component {
             url: `/api/users/${this.props.currentUser._id}/stocks`
         }).then((res) => {
             this.setState({
-                stocks: res.data
+                stocks: res.data,
+                // companyImage: `https://logo.clearbit.com/${res.data[2].name}.com`,
             })
         })
     }
@@ -29,32 +31,58 @@ class MyReport extends React.Component {
         // console.log(this.props.currentUser) // test to see current user
         return (
             <div className="MyReport">
-                <h1>My Report</h1>
+                {/* NEW */}
+                <header id="gtco-header" className="gtco-cover gtco-cover-sm" role="banner" >
+                    <div className="overlay"></div>
+                    <div className="gtco-container">
+                        <div className="row">
+                            <div className="col-md-12 col-md-offset-0 text-center">
+                                <div className="row row-mt-15em">
+                                    <div className="col-md-12 mt-text" data-animate-effect="fadeInUp">
+                                        <h1>My Report</h1>	
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>
                 
-                <Link to="/create-stock">
-                <button className="btn btn-light">Add a Stock</button>
-                </Link>
-                
-                <hr/>
-                <h2>Saved Stocks</h2>
-                    <ul>
+                <div className="gtco-section">
+                    <div className="gtco-container">
+                        <div className="row">
+                            <div className="col-md-8 col-md-offset-2 text-center gtco-heading">
+                                <Link to="/create-stock">
+                                <button className="btn btn-primary">Add a Stock</button>
+                                </Link>				
+                            </div>
+                        </div>
+                        {/* Saved Stocks */}
+                        <div className="row">
                         {this.state.stocks.map((stock, index) => {
                             return (
-                                <div>
-                                    <Link to={`/show-stock/${stock._id}`} key={index}>
-                                    <button className="btn btn-light" key={stock._id}>{stock.name}</button>
-                                    </Link>
-                                    
-                                    <br/>                                    
-                                    <br/>
+                            <div className="col-lg-4 col-md-4 col-sm-6">
+                                <div className="fh5co-card-item image-popup">
+                                    <figure>
+                                        <img src="images/img_1.jpg" className="img-responsive"></img>
+                                        {/* Clearbit - try to get logo */}
+                                        {/* <img src={this.state.companyImage} className="img-responsive"/> */}
+                                    </figure>
+                                    <div className="fh5co-text">
+                                        <h2>{stock.name}</h2>
+                                        <Link to={`/show-stock/${stock._id}`} key={index}>                                        
+                                            <button className="btn btn-primary" key={stock._id}>Details</button>                                            
+                                        </Link>
+                                    </div>
                                 </div>
+                            </div>
                             ) 
                         })}
-                    </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
-
 } //last bracket
 
 export default MyReport
